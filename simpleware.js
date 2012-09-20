7fb4fdddad847e8e4c3e85dadad98ae442e5a95e
Kevin McTigue <firefoxman1@gmail.com>
2012-09-20 12:41:54 -0400

    Performance optimizations like _next in mw() and categorizing routes so loops are shorter when matching.

--- simpleware.js
+++ simpleware.js
@@ -6,48 +6,55 @@ var concat = Array.prototype.concat;
 var SHORTCUT_METHODS = ['get', 'post', 'head', 'options', 'put', 'delete'];
 
 function createRouter() {
-	var routes = [];
+	var routes = {};
 	var router = dispatch.bind(null, routes);
-	
+
 	router.request = request.bind(null, routes);
 	SHORTCUT_METHODS.forEach(function(method) {
-		router[method] = request.bind(null, routes, method.toUpperCase());
+		router[method] = request.bind(null, routes, method);
 	});
-	
+
 	return router;
 }
 
 function request(routes, method, pattern) {
-	var args = slice.call(arguments, 3);
-	
-	routes.push({
-		method: method.toUpperCase(),
+  method = method.toUpperCase();
+
+	var args = slice.call(arguments, 3),
+      routeMethod = routes[method] || (routes[method] = []);
+
+	routeMethod.push({
+		method: method,
 		pattern: pattern,
-		handler: args.length > 1 ? mw.apply(null, args) : args[0]
+		handler: args.length > 1 ? mw(args) : args[0]
 	});
 }
 
 function dispatch(routes, req, res, next) {
-	var parts = url.parse(req.url);
+	var parts = url.parse(req.url),
+      method = req.method.toUpperCase(),
+      routeMethod = routes[method];
+
+  if (!routeMethod) return false; //If there are no routes for the current method, why bother?
+
 	req.path = parts.pathname;
 	if(parts.query) { // Does query parsing
 		req.query = qs.parse(parts.query);
 	}
-	
-	var method = req.method.toUpperCase();
-	
-	for(var i = 0; i < routes.length; i++) {
-		var route = routes[i];
-		if(route.method != method) continue;
-		
-		if(typeof route.pattern != 'string') {
+
+	for(var i = 0, l = routeMethod.length; i < l; i++) {
+		var route = routeMethod[i];
+
+		//if(route.method != method) continue;
+
+		if(typeof route.pattern !== 'string') {
 			var m = req.path.match(route.pattern);
 			if(m != null) {
 				req.params = m.slice(1); // Gets matched parameters from regex routes
 				route.handler(req, res, next);
 				return;
 			}
-		} else if(req.path == route.pattern) {
+		} else if(req.path === route.pattern) {
 			route.handler(req, res, next);
 			return;
 		}
@@ -58,14 +65,16 @@ function dispatch(routes, req, res, next) {
 
 function mw() { // Middleware
 	var list = concat.apply([], arguments);
-	
+
 	return function(req, res) {
-		var wares = list.slice(0);
+		var listPlace = 0,
+        listLength = list.length;
+
 		_next();
-		
+
 		function _next() {
-			if(wares.length > 0) {
-				wares.shift()(req, res, _next);
+			if (listPlace < listLength) {
+				list[listPlace++](req, res, _next);
 			}
 		}
 	}
